2b64707988eca6b96425a3a9ac5d86a86f739c89
Joseph Chiou <jochiou0601@gmail.com>
2020-05-16 05:51:00 +0800

    init commit

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,81 @@
+package helloworld;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<Object, Object> {
+
+    public Object handleRequest(final Object input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        try {
+            //final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            String output = createPresignedUrl("test-file-name.txt");
+                    //String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+
+            return new GatewayResponse(output, headers, 200);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new GatewayResponse("{}", headers, 500);
+        }
+    }
+
+    private String getPageContents(String address) throws IOException {
+        URL url = new URL(address);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    private String createPresignedUrl(String fileSignature) throws AmazonServiceException, SdkClientException, IOException {
+        Regions clientRegion = Regions.US_WEST_2;
+        String bucketName = "public-file-storage-oregon";
+        String objectKey = "testFile.txt";
+
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider())
+                .withRegion(clientRegion)
+                .build();
+
+        // Set the pre-signed URL to expire after one hour.
+        java.util.Date expiration = new java.util.Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60;
+        expiration.setTime(expTimeMillis);
+
+        // Generate the pre-signed URL.
+        System.out.println("Generating pre-signed URL.");
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, objectKey)
+                .withMethod(HttpMethod.PUT)
+                .withExpiration(expiration);
+
+        URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        System.out.println("URL = " + url.toString());
+
+        return url.toString();
+    }
+}
